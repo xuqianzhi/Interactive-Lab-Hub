32ab3bf7a7e82eee32d5bc449e71487b5f339e72
xuqianzhi <xuqianzhi1998@hotmail.com>
2021-09-19 16:50:33 -0400

    lab2 code

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -4,6 +4,7 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
+from time import strftime, sleep
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
 cs_pin = digitalio.DigitalInOut(board.CE0)
@@ -60,12 +61,33 @@ backlight = digitalio.DigitalInOut(board.D22)
 backlight.switch_to_output()
 backlight.value = True
 
+button = digitalio.DigitalInOut(board.D23)
+button.switch_to_input()
+
+# my design use the duration of the song numb by Linkin Park 
+numb_in_second = 188
+
 while True:
     # Draw a black filled box to clear the image.
     draw.rectangle((0, 0, width, height), outline=0, fill=0)
 
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
-
+    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
+    if button.value:
+        hour = int(strftime("%H"))
+        minute = int(strftime("%M"))
+        second = int(strftime("%S"))
+        time_in_second = hour * 3600 + minute * 60 + second
+        num_of_numb = time_in_second / numb_in_second
+        draw.text((0, 0), strftime("%m/%d/%Y"), font=font, fill="#FF00FF")
+        draw.text((0, 20), "The song, Numb", font=font, fill="#FF00FF")
+        draw.text((0, 40), "by Linkin Park", font=font, fill="#FF00FF")
+        draw.text((0, 60), "has been played", font=font, fill="#FF00FF")
+        draw.text((0, 80), "{:.2f}".format(num_of_numb), font=font, fill="#FF0000")
+        draw.text((60, 80), " times", font=font, fill="#FF00FF")
+        draw.text((0, 100), "since midnight of today", font=font, fill="#FF00FF")
+    else:
+        draw.text((0, 0), "where each numb lasts", font=font, fill="#FF00FF")
+        draw.text((0, 20), "3:08", font=font, fill="#FF0000")
     # Display image.
     disp.image(image, rotation)
     time.sleep(1)
