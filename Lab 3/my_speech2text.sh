14789c306636e8d3481c45edf1946b736a671b49
Larry Shu <40366675+xuqianzhi@users.noreply.github.com>
2021-10-04 18:39:50 -0400

    Update my_speech2text.sh

--- Lab 3/my_speech2text.sh
+++ Lab 3/my_speech2text.sh
@@ -2,5 +2,5 @@
 
 flite -voice slt -t "Please tell me your phone number."
 
-arecord -D hw:1,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
+arecord -D hw:3,0 -f cd -c1 -r 48000 -d 5 -t wav recorded_mono.wav
 python3 test_words.py recorded_mono.wav
