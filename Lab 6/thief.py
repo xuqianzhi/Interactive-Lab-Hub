eea361d272db50bd7d774c421b463c5b53d2f135
xuqianzhi <xuqianzhi1998@hotmail.com>
2021-11-15 20:46:32 -0500

    lab6

--- Lab 6/thief.py
+++ Lab 6/thief.py
@@ -21,6 +21,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
 
 sensor.enable_proximity = True
+sensor.enable_color = True
 threshold = 125
 
 topic = "IDD/detect"  
