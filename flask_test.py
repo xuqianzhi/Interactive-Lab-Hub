b998583ddc9ade5fa719e05709f66448ff356936
Larry Shu <40366675+xuqianzhi@users.noreply.github.com>
2021-12-04 00:28:49 -0500

    Create flask_test.py

---
+++ flask_test.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, Response
+import time
+import random
+import threading
+
+app = Flask(__name__)
+
+@app.route('/humidity')
+def humidity():
+
+    data = random.uniform(0, 1)
+
+    def long_running_get_data(**kwargs):
+        your_params = kwargs.get('post_data', {})
+
+        time.sleep(5)
+
+    thread = threading.Thread(target=long_running_get_data, kwargs={
+                    'post_data': data})
+    thread.start()
+    response = jsonify({"humidity": data})
+    response.headers.add('Access-Control-Allow-Origin', '*') 
+    return response
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host="192.168.99.160", port=4000)
