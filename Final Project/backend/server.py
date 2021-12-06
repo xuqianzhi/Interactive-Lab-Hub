afdd1631a8dc5aafc01a109b6b541d8c3ff3563f
xuqianzhi <xuqianzhi1998@hotmail.com>
2021-12-06 16:55:57 -0500

    frontend

--- Final Project/backend/server.py
+++ Final Project/backend/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/humidity')
 def humidity():
 
-    data = random.uniform(0, 1)
+    data = random.uniform(0, 20)
 
     def long_running_get_data(**kwargs):
         your_params = kwargs.get('post_data', {})
@@ -20,5 +20,21 @@ def humidity():
     response.headers.add('Access-Control-Allow-Origin', '*') 
     return response
 
+@app.route('/temperature')
+def temperature():
+
+    data = random.uniform(30, 80)
+
+    def long_running_get_data(**kwargs):
+        your_params = kwargs.get('post_data', {})
+
+    thread = threading.Thread(target=long_running_get_data, kwargs={
+                    'post_data': data})
+    thread.start()
+    response = jsonify({"temperature": data})
+    response.headers.add('Access-Control-Allow-Origin', '*') 
+    return response
+
 if __name__ == "__main__":
     # app.run(host="<Your PI IP address here>", port=4000)
+    app.run(host="192.168.99.54", port=4000)
